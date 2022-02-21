bd882b83a84432d1ba7642c07241d52c9d4b578f
Paulina Segovia <paulinasegovia@gmail.com>
2022-02-21 11:37:35 -0800

    commit

---
+++ js/script.js
@@ -0,0 +1,270 @@
+//STEP 1
+
+// let some_month; 
+// let someMonth;
+
+// function the Month();
+// function theMonth();
+
+// let current-month;
+// let currentMonth;
+
+// let summer_month;
+// let summerMonth;
+
+// let MyLibrary-function;
+// let myLibraryFunction;
+
+
+
+//STEP 2
+
+// //numeric literal expression
+// let age = 41;
+// console.log(age);
+
+// //string literal expression
+// let myName = "Paulina Segovia";
+// console.log(myName);
+
+// //a Boolean literal expression
+// let myName = "Paulina Segovia";
+// let lovesReading = true;
+// console.log(lovesReading);
+
+// //null literal expression
+// let myName = null;
+// console.log(myName);
+
+
+
+//STEP 3
+// complex/variable expressions
+//example 1
+// let x = 10;
+// let y = 20;
+// let total = (x + y)/2;
+// console.log(total);
+
+//example 2
+// let x = 1;
+// let y = 40;
+// let paulinaAge = (x + y);
+// console.log("Paulina is " + paulinaAge + " years old.");
+
+
+
+//STEP 4
+/*First is Camel Case, the second is Hungarian notation*/
+// let firstName;
+// let strFirstName;
+
+// let lastName;
+// let strLastName;
+
+// let address;
+// let strAddress;
+
+// let city;
+// let strCity;
+
+// let state;
+// let strState;
+
+// let zipCode;
+// let numZipCode;
+
+// let myAge;
+// let numMyAge;
+
+// let referralSource; /*(don't know what is this)*/
+// let strReferralSource; 
+
+// let mayWeContactYou;
+// let strMayWeContactYou;
+
+
+
+
+//STEP 5
+
+// let myAge;
+// myAge = 41;
+
+// let firstNamel;
+// firstName = "Paulina";
+
+// let state;
+// state = "California";
+
+
+// let myAge = 41;
+// let firstName = "Paulina";
+// let state = "California";
+
+
+// let myAge = 41, firstName = "Paulina", state = "California";
+
+
+
+
+//STEP 6
+
+// let myName = "Paulina";
+// let myAge = 41;
+// document.write(myName + " is " + myAge + " years old.");
+// console.log(typeof(myName)); /* I included the typeof to check each variable. Could I have included this in one line statement? I tried but it didn;t work */
+// console.log(typeof(myAge));
+
+
+
+
+//STEP 7
+
+// Create two variables. 
+// For the first variable, add a Boolean and a string and display the coerced result. 
+
+// let x = "Marcia";
+// if (x == "Paulina")
+// document.write("The name is ", x);
+// else {
+//     document.write("You got a different name");
+// }
+
+// let x = "Paulina";
+// if (x == "Paulina")
+// document.write("The name is ", x);
+// else {
+//     document.write("You got a different name");
+// }
+
+
+// For the second variable, add a number and a Boolean and display the coerced result. 
+// let x = 3;
+// if (x == 5)
+// document.write("You got the number ", x, ". That is the right number!")
+// else {
+//     document.write("Keep trying!")
+// }
+
+// let x = 5;
+// if (x == 5)
+// document.write("You got the number ", x, ". That is the right number!")
+// else {
+//     document.write("Keep trying!")
+// }
+
+
+
+
+// //STEP 8
+
+// Is the following string literal valid? If not, how would you fix it?
+// let someString = 'Who once said, "Only two things are infinite, the universe 
+// and human stupidity, and I'm not sure about the former."';
+// window.console.log(someString);
+
+// It is NOT Valide. The valide one is the following: 
+// let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+// window.console.log(someString);
+
+
+
+
+// //STEP 9
+// Create a variable that produces a null value in the console window. 
+// let x = null;
+// console.log(x);
+
+
+// Then, create a variable that produces an undefined value in the console window.
+// let x;
+// console.log(x);
+
+
+
+
+// //STEP 10
+// Use the unary typeof operator on various literals to return the following types within the console window: string, 
+// number, Boolean, object, and undefined.
+
+// let name = "Paulina";
+// console.log(typeof(name));
+
+// let x = 1;
+// console.log(typeof(x));
+
+// let y = true;
+// console.log(typeof(y));
+
+// const paulina = {
+//     firstName: "Paulina",
+//     lastName: "Segovia",
+//     age: 41,
+// };
+// console.log(typeof(paulina));
+
+let z;
+console.log(typeof(z));
+
+
+
+
+// //STEP 11
+// Within an alert box, use the concatenation operator (+) to display text in the alert box that appears as follows: 
+// Hello Zak Ruvalcaba, welcome to the JavaScript class!
+// Substitute my name for your name. Although not necessary in practice, I want you to use 2 concatenation operators 
+// to construct this string of text. One after the text “Hello” and a second one after your name and before the comma.
+
+
+// //STEP 12
+// Declare a variable called name and set it equal to your name. 
+// Substitute your name in the previous alert string with the variable instead.
+
+// //STEP 13
+// Declare a variable called course and set it equal to “JavaScript”. 
+// Rework your alert string so that it displays the string of text but using the variables as opposed to hard coded text.
+
+// //STEP 14
+
+// Rework the above string so that a line break is added right before the word “Welcome”. Your alert box should 
+// display as follows:
+// Hello Zak Ruvalcaba.
+// Welcome to the JavaScript class!
+
+
+// //STEP 15
+// Replace the hardcoded string of your name with a prompt that asks the user for their name. The prompt’s response 
+// will now be captured in the name variable. 
+
+// //STEP 16
+// Replace the hardcoded string of the class you are taking with a prompt that asks the user for the class they are 
+// taking. The prompt’s response will now be captured in the course variable.
+
+// //STEP 17
+// Declare a variable called x and assign it a value of 10. 
+// Declare a variable called y and assign it a value of 20. 
+// Display the sum of those two numbers in the console window. 
+
+// //STEP 18
+// Declare a variable called x and assign it a value of 20. 
+// Add and assign 20 to that variable and display the result in the console window.
+// The result should be 40.
+
+// //STEP 19
+// Declare a variable called x and assign it a value of 20.
+// Multiply and assign 5 to that variable and display the result in the console window. 
+// The result should be 100.
+
+// //STEP 20
+// Declare a variable called x and assign it a value that equals the remainder of 20 divided by 3.
+// Divide and assign 1 to that variable and display the result in the console window. 
+// The result should be 2. If you got 6.66 try again.
+
+// //STEP 21
+// Using a set of Comparison and Logical operators, write an application that evaluates to true and displays the result 
+// within the console window.
+
+// //STEP 22
+// Using a set of Comparison and Logical operators, write an application that evaluates to false and displays the result 
+// within the console window. The application cannot use the same operators used in the previous application.
